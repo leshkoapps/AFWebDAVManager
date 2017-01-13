1f3a88783e18fb9c669436cb0616df2d146a7265
Artem Meleshko <ameleshko@ameleshko.local>
2017-01-13 12:53:49 +0200

    WebDav Response Namespace not always 'D' I removed the inNamespace:@"D" for the AFWebDAVMultiStatusResponse because the response does not always use the "D" Namespace I found that Microsoft servers use the namespace "a".  Information on Microsoft webdav https://msdn.microsoft.com/en-us/library/ms879496(v=exchg.65).aspx
    I testested with an "a" namespace and a "D" namespace server and there were no issues.

--- AFWebDAVManager/AFWebDAVManager.m
+++ AFWebDAVManager/AFWebDAVManager.m
@@ -525,21 +525,148 @@ static NSString * AFWebDAVStringForLockType(AFWebDAVLockType type) {
 @property (readwrite, nonatomic, assign) NSUInteger contentLength;
 @property (readwrite, nonatomic, copy) NSDate *creationDate;
 @property (readwrite, nonatomic, copy) NSDate *lastModifiedDate;
+
+
+//! add by OYXJ, used to retrieve some wanted data of XML.
+@property (readwrite, nonatomic, strong) ONOXMLElement *element;//strong
+
+
+/**
+ begin --- 实现协议 WebDavResource --- begin
+ */
+
+//! The `etag` of the resource at the response URL.
+@property(nonatomic, copy, readwrite) NSString *etag;
+
+/**
+ 服务端资源的唯一id(主键)
+ */
+@property(nonatomic, copy, readwrite) NSString *name; // 相当于 source id
+
+//@property(nonatomic, copy, readonly) NSString *ctag;
+
+@property(nonatomic, strong, readwrite)NSDictionary<NSString*, NSString*> *customProps;
+
+//private final Resourcetype resourceType; ???
+//private final String contentType; ??? TODO::
+//private final Long contentLength; ??? TODO::
+
+@property(nonatomic, copy, readwrite) NSString *notedata;
+@property(nonatomic, copy, readwrite) NSString *lastModified;
+@property(nonatomic, copy, readwrite) NSString *deletedTime;
+@property(nonatomic, copy, readwrite) NSString *deletedDataName;
+@property(nonatomic, copy, readwrite) NSString *deleted;
+
+/**
+ end --- 实现协议 WebDavResource --- end
+ */
+
+
+
 @end
+
+
 
 @implementation AFWebDAVMultiStatusResponse
 
+// by OYXJ
+NSString * const getcontentlengthCONST = @"getcontentlength";
+NSString * const creationdateCONST = @"creationdate";
+NSString * const getlastmodifiedCONST = @"getlastmodified";
+NSString * const getetagCONST = @"getetag";//实现协议 WebDavResource
+
+// 实现协议 WebDavResource
+NSString * const resourcetypeCONST = @"resourcetype";
+NSString * const getcontenttypeCONST = @"getcontenttype";
+NSString * const notedataCONST = @"notedata";
+NSString * const getDeletedTimeCONST = @"getDeletedTime";
+NSString * const getDeletedDataNameCONST = @"getDeletedDataName";
+NSString * const getDeletedCONST = @"getDeleted";
+
+
+#pragma mark - init
+
 - (instancetype)initWithResponseElement:(ONOXMLElement *)element {
     NSParameterAssert(element);
-
-    NSString *href = [[element firstChildWithTag:@"href" inNamespace:@"D"] stringValue];
-    NSInteger status = [[[element firstChildWithTag:@"status" inNamespace:@"D"] numberValue] integerValue];
-
+    
+    
+    /*
+     <d:response>
+     <d:href>/sync/chatcontacts/f5187830e27a4120bd17107c62011ba5</d:href>
+     <d:propstat>
+     <d:prop>
+     <d:getetag>W/"db3b4d0b9c05509d3967a56b4a0a0353"</d:getetag>
+     <x2:chatcontacts-data xmlns:x2="urn:ietf:params:xml:ns:webdav">{"source_id":"f5187830e27a4120bd17107c62011ba5","display_name":"vhs","phone_number":"13716750071#13716750075","is_voip_number":"1","account_phone_number":"13661248236","contact_type":0,"contact_from":0,"device_id":"--866647020047438"}</x2:chatcontacts-data>
+     </d:prop>
+     <d:status>HTTP/1.1 200 OK</d:status>
+     </d:propstat>
+     </d:response>
+     */
+    
+    
+    /**
+     WebDav Response Namespace not always 'D'
+     Ref.:  https://github.com/BitSuites/AFWebDAVManager/commit/c25abdb71e07897212b44212e2d854e744a64048
+     rocket0423 committed on 10 Jul 2015
+     1 parent 45504c7 commit c25abdb71e07897212b44212e2d854e744a64048
+     
+     NSString *href = [[element firstChildWithTag:@"href" inNamespace:@"D"] stringValue];
+     NSInteger status = [[[element firstChildWithTag:@"status" inNamespace:@"D"] numberValue] integerValue];
+     */
+    NSString *href = [[element firstChildWithTag:@"href"] stringValue];
+    NSInteger status = [[[element firstChildWithTag:@"status"] numberValue] integerValue];
+    
+    if (status == 0) {//[begin] fix bug: ｀status code｀ not found in firstChild element.
+        NSString *statusString = [[[element firstChildWithTag:@"propstat"] firstChildWithTag:@"status"] stringValue];
+        statusString = [statusString stringByReplacingOccurrencesOfString:@"HTTP/1.1" withString:@""];
+        statusString = [statusString stringByTrimmingCharactersInSet:[[NSCharacterSet alphanumericCharacterSet] invertedSet]];
+        statusString = [statusString stringByTrimmingCharactersInSet:[NSCharacterSet letterCharacterSet]];
+        statusString = [statusString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+        if (statusString.length > 0) {
+            if ([statusString integerValue] > 0) {
+                status = [statusString integerValue];
+            }
+        }
+    }//[end] fix bug: ｀status code｀ not found in firstChild element.
+    
+    
     self = [self initWithURL:[NSURL URLWithString:href] statusCode:status HTTPVersion:@"HTTP/1.1" headerFields:nil];
+    {//element
+        //TODO: A true deep copy  http://stackoverflow.com/questions/647260/deep-copying-an-nsarray
+        self.element = element;//strong reference
+        {//解析数据---begin//
+            NSDictionary *atts = [element attributes];
+            if (atts.count > 0) {
+                //开源库Ono
+                self.customProps = atts;
+            }else{
+                //自己 解析数据
+                ONOXMLElement *propElement = [[element firstChildWithTag:@"propstat"] firstChildWithTag:@"prop"];
+                
+                
+                NSMutableDictionary<NSString*,NSString*> *proDic = [NSMutableDictionary dictionaryWithCapacity:2];
+                
+                NSArray<ONOXMLElement*> *childrenElements = [propElement children];
+                for (int i = 0; i < childrenElements.count; i++) {
+                    ONOXMLElement *e = childrenElements[i];
+                    NSString *aTag = [e tag];
+                    NSString *stringValue = [self valueOfTag: aTag
+                                                   inElement: e];
+                    if (aTag.length && stringValue.length) {
+                        [proDic setObject:stringValue forKey:aTag];
+                    }
+                }
+                
+                //DDLogVerbose(@"%@", proDic);
+                
+                self.customProps = proDic;
+            }
+        }//解析数据---end//
+    }//element
     if (!self) {
         return nil;
     }
-
+    
     ONOXMLElement *propElement = [[element firstChildWithTag:@"propstat"] firstChildWithTag:@"prop"];
     for (ONOXMLElement *resourcetypeElement in [propElement childrenWithTag:@"resourcetype"]) {
         if ([resourcetypeElement childrenWithTag:@"collection"].count > 0) {
@@ -547,12 +674,279 @@ static NSString * AFWebDAVStringForLockType(AFWebDAVLockType type) {
             break;
         }
     }
-
-    self.contentLength = [[[propElement firstChildWithTag:@"getcontentlength" inNamespace:@"D"] numberValue] unsignedIntegerValue];
-    self.creationDate = [[propElement firstChildWithTag:@"creationdate" inNamespace:@"D"] dateValue];
-    self.lastModifiedDate = [[propElement firstChildWithTag:@"getlastmodified" inNamespace:@"D"] dateValue];
-
+    
+    
+    /**
+     WebDav Response Namespace not always 'D'
+     Ref.:  https://github.com/BitSuites/AFWebDAVManager/commit/c25abdb71e07897212b44212e2d854e744a64048
+     rocket0423 committed on 10 Jul 2015
+     1 parent 45504c7 commit c25abdb71e07897212b44212e2d854e744a64048
+     
+     self.contentLength = [[[propElement firstChildWithTag:@"getcontentlength" inNamespace:@"D"] numberValue] unsignedIntegerValue];
+     self.creationDate = [[propElement firstChildWithTag:@"creationdate" inNamespace:@"D"] dateValue];
+     self.lastModifiedDate = [[propElement firstChildWithTag:@"getlastmodified" inNamespace:@"D"] dateValue];
+     */
+    
+    
+    //by OYXJ
+    NSString *ns = [propElement namespace];
+    NSMutableArray<NSString*> *beginEndTAGs = [NSMutableArray arrayWithCapacity:2];
+    NSArray * const tags = @[getcontentlengthCONST,creationdateCONST,getlastmodifiedCONST,getetagCONST];
+    [tags enumerateObjectsUsingBlock:^(id  _Nonnull eachTag, NSUInteger idx, BOOL * _Nonnull stop) {
+        NSString *beginTAG = [NSString stringWithFormat:@"<%@:%@>", ns,eachTag];
+        NSString *endTAG  = [NSString stringWithFormat:@"</%@:%@>", ns,eachTag];
+        [beginEndTAGs addObject: beginTAG];
+        [beginEndTAGs addObject: endTAG];
+    }];
+    
+    
+    {//getcontentlength
+        self.contentLength = [[[propElement firstChildWithTag:getcontentlengthCONST]
+                               numberValue] unsignedIntegerValue];
+        if (self.contentLength==0) {//by OYXJ
+            NSMutableString *contentLengthSTR = [[[propElement firstChildWithTag:getcontentlengthCONST]
+                                                  stringValue] mutableCopy];
+            [beginEndTAGs enumerateObjectsUsingBlock:^(id  _Nonnull aTAG, NSUInteger idx, BOOL * _Nonnull stop) {
+                [contentLengthSTR replaceOccurrencesOfString:aTAG
+                                                  withString:@""
+                                                     options:NSLiteralSearch
+                                                       range:NSMakeRange(0, contentLengthSTR.length)];
+            }];
+            
+            NSNumber *aContentLength = [propElement.document.numberFormatter numberFromString:contentLengthSTR];
+            
+            self.contentLength = [aContentLength unsignedIntegerValue];
+        }
+    }//getcontentlength
+    
+    {//creationdate
+        self.creationDate = [[propElement firstChildWithTag:creationdateCONST] dateValue];
+        if (self.creationDate==nil) {//by OYXJ
+            NSMutableString *creationDateSTR = [[[propElement firstChildWithTag:creationdateCONST]
+                                                 stringValue] mutableCopy];
+            [beginEndTAGs enumerateObjectsUsingBlock:^(id  _Nonnull aTAG, NSUInteger idx, BOOL * _Nonnull stop) {
+                [creationDateSTR replaceOccurrencesOfString:aTAG
+                                                 withString:@""
+                                                    options:NSLiteralSearch
+                                                      range:NSMakeRange(0, creationDateSTR.length)];
+            }];
+            
+            NSDate *aCreationDate = [propElement.document.dateFormatter dateFromString:creationDateSTR];
+            if (aCreationDate==nil) {
+                NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+                //[dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"]];//by OYXJ
+                //[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];//by OYXJ
+                [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];//by OYXJ
+                
+                aCreationDate = [dateFormatter dateFromString:creationDateSTR];
+            }
+            
+            self.creationDate = aCreationDate;
+        }
+    }//creationdate
+    
+    
+    {//getlastmodified
+        self.lastModifiedDate = [[propElement firstChildWithTag:getlastmodifiedCONST] dateValue];
+        if (self.lastModifiedDate==nil) {//by OYXJ
+            NSMutableString *lastModifiedDateSTR = [[[propElement firstChildWithTag:getlastmodifiedCONST]
+                                                     stringValue] mutableCopy];
+            [beginEndTAGs enumerateObjectsUsingBlock:^(id  _Nonnull aTAG, NSUInteger idx, BOOL * _Nonnull stop) {
+                [lastModifiedDateSTR replaceOccurrencesOfString:aTAG
+                                                     withString:@""
+                                                        options:NSLiteralSearch
+                                                          range:NSMakeRange(0, lastModifiedDateSTR.length)];
+            }];
+            
+            NSDate *aLastModifiedDate = [propElement.document.dateFormatter dateFromString:lastModifiedDateSTR];
+            if (aLastModifiedDate==nil) {
+                NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+                //[dateFormatter setLocale:[[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"]];//by OYXJ
+                //[dateFormatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ssZ"];//by OYXJ
+                [dateFormatter setDateFormat:@"yyyy-MM-dd HH:mm:ss"];//by OYXJ
+                
+                aLastModifiedDate = [dateFormatter dateFromString:lastModifiedDateSTR];
+            }
+            
+            self.lastModifiedDate = aLastModifiedDate;
+        }
+    }//getlastmodified
+    
+    
+    {//getetag
+        NSMutableString *aEtagSTR = [[[propElement firstChildWithTag:getetagCONST]
+                                      stringValue] mutableCopy];
+        [beginEndTAGs enumerateObjectsUsingBlock:^(id  _Nonnull aTAG, NSUInteger idx, BOOL * _Nonnull stop) {
+            [aEtagSTR replaceOccurrencesOfString:aTAG
+                                      withString:@""
+                                         options:NSLiteralSearch
+                                           range:NSMakeRange(0, aEtagSTR.length)];
+        }];
+        
+        self.etag = [aEtagSTR copy];
+    }//getetag
+    
+    
     return self;
 }
+
+
+#pragma mark - private
+
+/**
+ 根据标签名字，获取标签的值
+ 特别注意，标签的值，是String类型，才使用此方法。
+ 
+ @param aTagNameCONST 标签名字，使用此类中定义的 常量字符串
+ 
+ @return 标签的值
+ */
+- (NSString *)valueOfTag:(NSString *)aTagNameCONST inElement:(ONOXMLElement *)anElement
+{
+    if (aTagNameCONST.length <= 0) {
+        return nil;
+    }
+    if (anElement == nil) {
+        return nil;
+    }
+    
+    NSString *returnStr = nil;
+    @try {
+        //Code that can potentially throw an exception
+        
+        NSMutableString *aTagValueSTR = [[anElement stringValue] mutableCopy];
+        
+        
+        if ([aTagValueSTR rangeOfString:aTagNameCONST].location == NSNotFound) {
+            // do nothing here !
+        }else{
+            
+            NSString *ns = [anElement namespace];
+            NSMutableArray *beginEndTAGs = [NSMutableArray arrayWithCapacity:1];
+            [@[aTagNameCONST] enumerateObjectsUsingBlock:^(id  _Nonnull eachTag, NSUInteger idx, BOOL * _Nonnull stop) {
+                NSString *beginTAG = [NSString stringWithFormat:@"<%@:%@>", ns,eachTag];
+                NSString *endTAG  = [NSString stringWithFormat:@"</%@:%@>", ns,eachTag];
+                [beginEndTAGs addObject: beginTAG];
+                [beginEndTAGs addObject: endTAG];
+            }];
+            
+            [beginEndTAGs enumerateObjectsUsingBlock:^(id  _Nonnull aTAG, NSUInteger idx, BOOL * _Nonnull stop) {
+                [aTagValueSTR replaceOccurrencesOfString:aTAG
+                                              withString:@""
+                                                 options:NSLiteralSearch
+                                                   range:NSMakeRange(0, aTagValueSTR.length)];
+            }];
+        }
+        
+        returnStr = [aTagValueSTR copy];
+        
+    } @catch (NSException *exception) {
+        //Handle an exception thrown in the @try block
+        
+        NSLog(@"%@", exception);
+        
+    } @finally {
+        //Code that gets executed whether or not an exception is thrown
+        
+        return returnStr;
+    }
+    
+}
+
+
+#pragma mark - getters
+
+- (NSString *)etag
+{
+    return _etag;
+}
+
+/**
+ 服务端资源的唯一id(主键)
+ 相当于 source id
+ */
+- (NSString *)name
+{
+    if (nil==_name) {
+        _name = self.URL.absoluteString.lastPathComponent ?: self.URL.absoluteString;
+    }
+    
+    return _name;
+}
+
+//- (NSString *)ctag
+//{
+//    return _ctag;
+//}
+
+- (NSDictionary<NSString*,NSString*> *)customProps
+{
+    return _customProps;
+}
+
+//private final Resourcetype resourceType; ???
+//private final String contentType; ??? TODO::
+//private final Long contentLength; ??? TODO::
+
+
+
+- (NSString *)notedata
+{
+    if (nil==_notedata) {
+        
+        ONOXMLElement *propElement = [[self.element firstChildWithTag:@"propstat"] firstChildWithTag:@"prop"];
+        _notedata = [self valueOfTag: notedataCONST
+                           inElement: [propElement firstChildWithTag:notedataCONST]];
+    }
+    
+    return _notedata;
+}
+
+- (NSString *)lastModified
+{
+    if (nil==_lastModified) {
+        
+        _lastModified = [self.lastModifiedDate description];
+    }
+    
+    return _lastModified;
+}
+
+- (NSString *)deletedTime
+{
+    if (nil==_deletedTime) {
+        
+        ONOXMLElement *propElement = [[self.element firstChildWithTag:@"propstat"] firstChildWithTag:@"prop"];
+        _deletedTime = [self valueOfTag: getDeletedTimeCONST
+                              inElement: [propElement firstChildWithTag:getDeletedTimeCONST]];
+    }
+    
+    return _deletedTime;
+}
+
+- (NSString *)deletedDataName
+{
+    if (nil==_deletedDataName) {
+        
+        ONOXMLElement *propElement = [[self.element firstChildWithTag:@"propstat"] firstChildWithTag:@"prop"];
+        _deletedDataName = [self valueOfTag: getDeletedDataNameCONST
+                                  inElement: [propElement firstChildWithTag:getDeletedDataNameCONST]];
+    }
+    
+    return _deletedDataName;
+}
+
+- (NSString *)deleted
+{
+    if (nil==_deleted) {
+        
+        ONOXMLElement *propElement = [[self.element firstChildWithTag:@"propstat"] firstChildWithTag:@"prop"];
+        _deleted = [self valueOfTag: getDeletedCONST
+                          inElement: [propElement firstChildWithTag:getDeletedCONST]];
+    }
+    
+    return _deleted;
+}
+
 
 @end
